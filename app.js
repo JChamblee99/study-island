d7462c60bb7c81dd741e8d90fbb2536a765b4537
John Chamblee <42752805+JChamblee99@users.noreply.github.com>
2022-03-04 15:23:42 -0600

    Cloudflare Isolation (#7)
    * Added cloudflare IP checking library
    
    * Added Cloudflare isolation to prevent bypasses
    
    * Added error 418 to Cloudflare bypasses
    
    * Added staging and development apps to Cloudflare
    
    * Added package-lock.json
    
    * Updated package-lock.json
    
    * Fixed app.json formatting

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
+
 const express = require('express');
+const cloudflareIp = require('cloudflare-ip');
 
 const app = express();
 
@@ -12,6 +14,23 @@ app.set('port', process.env.PORT || 3000);
 
 app.use(express.static(__dirname + '/public'));
 
+// Cloudflare isolation handler (middleware)
+app.use(function(req, res, next) {
+	// Cloudflare isolation is disabled for local development and review apps.
+	if (["production", "staging", "development"].indexOf(process.env.NODE_ENV) == -1)
+		return next();
+
+	// The last IP within the header is set by Heroku's load balancer.
+	// First IP is the source client, the second should be Cloudflare.
+	if (req.get('x-forwarded-for')) {
+		const fwd = req.get('x-forwarded-for').split(',');
+		if (fwd.length == 2 && cloudflareIp(fwd[1]))
+			return next();
+	}
+
+	res.status(418);
+	res.end();
+});
 
 app.get('/', function(req, res) {
 	res.json('Hello World!');
