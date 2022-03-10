aa76aa6bd9e341d4ddf92375d0ff5e2a5f715f75
JChamblee99 <jtc38@students.uwf.edu>
2022-03-10 04:36:52 -0600

    Created sample DB builder

---
+++ src/database/sample.js
@@ -0,0 +1,48 @@
+const Island = require('./models/island.model');
+const Thread = require('./models/thread.model');
+const Reply = require('./models/reply.model');
+const User = require('./models/user.model');
+
+module.exports.build = async () => {
+    const testUserData = {
+        firstName: 'John',
+        lastName: 'Doe',
+        username: 'johnDoe',
+        email: 'john@email.com',
+        password: 'password',
+        role: 'user'
+    };
+
+    const testUser = new User(testUserData);
+
+    const testReplyData = {
+        replyAuthor: testUser,
+        content: 'Content of the reply',
+    };
+
+    const testReply = new Reply(testReplyData);
+
+    const testThreadData = {
+        threadAuthor: testUser,
+        threadTitle: 'Thread Title',
+        content: 'Content of the thread',
+        replies: [testReply]
+    };
+
+    const testThread = new Thread(testThreadData);
+
+    const testIslandData = {
+        islandName: 'testIsland',
+        description: "This is a test island",
+        privacy: 'public',
+        users: [testUser],
+        threads: [testThread]
+    };
+
+    const testIsland = new Island(testIslandData);
+
+    await testUser.save();
+    await testReply.save();
+    await testThread.save();
+    await testIsland.save();
+}
