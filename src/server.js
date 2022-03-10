46691d7c94c684310c0d1dcefb2069b4a8327363
james <jwm52@students.uwf.edu>
2022-03-09 23:12:29 -0600

    formatted files

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 const app = require('./app');
 
-app.listen(app.get('port'), function(){
-    console.log( 'Express started on http://localhost:' +
-      app.get('port') + '; press Ctrl-C to terminate.' );
-  });
+app.listen(app.get('port'), function () {
+  console.log('Express started on http://localhost:' +
+    app.get('port') + '; press Ctrl-C to terminate.');
+});
