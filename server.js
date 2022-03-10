09065f0d4e184651490b842e72b01a23bcac531f
jwm52 <59706909+jwm52@users.noreply.github.com>
2022-03-10 01:14:44 -0600

    Handlebars integration (#11)
    * app architecture in place, start script adjusted
    
    * fixed architecture, fixed tests
    
    * cloudflare middleware refactored
    
    * added handlebar views and example json + html
    
    * home view worked on
    
    * formatted files
    
    * fix tabs
    
    * formatted and corrected example json models
    
    * redo, don't merge after midnight

--- server.js
+++
@@ -1,6 +0,0 @@
-const app = require('./app');
-
-app.listen(app.get('port'), function(){
-    console.log( 'Express started on http://localhost:' +
-      app.get('port') + '; press Ctrl-C to terminate.' );
-  });
