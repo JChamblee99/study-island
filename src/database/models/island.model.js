a462625c439fe74e8b2c54e6e83ccbccbedcde9a
JChamblee99 <jtc38@students.uwf.edu>
2022-03-10 02:13:53 -0600

    Fixed diff message

--- src/database/models/island.model.js
+++ src/database/models/island.model.js
@@ -12,13 +12,8 @@ const islandSchema = mongoose.Schema({
         enum: ['private', 'public'],
         default: 'public'
     },
-<<<<<<< HEAD
     users: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
     threads: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Thread' }]
-=======
-    users: [userSchema],
-    threads: []
->>>>>>> 6a2379dec1564ad5e04c009196fe6bc586d69898
 
 });
 
