a330c4243227152a825354d2bad3f7a5af3c941d
jwm52 <59706909+jwm52@users.noreply.github.com>
2022-03-10 01:05:54 -0600

    Handlebars integration (#8)
    * Create development.yml
    
    * separated server and app for testing purposes
    
    * adding testing folder
    
    * adding gitignore file
    
    * Update development.yml
    
    * Update package.json
    
    * Update package.json
    
    Adding jest and supertest dependencies
    
    * Update package.json
    
    * Update package.json
    
    * Update development.yml
    
    * Actually fix packagelock
    
    * Cloudflare Isolation (#7)
    
    * Added cloudflare IP checking library
    
    * Added Cloudflare isolation to prevent bypasses
    
    * Added error 418 to Cloudflare bypasses
    
    * Added staging and development apps to Cloudflare
    
    * Added package-lock.json
    
    * Updated package-lock.json
    
    * Fixed app.json formatting
    
    * app architecture in place, start script adjusted
    
    * fixed architecture, fixed tests
    
    * cloudflare middleware refactored
    
    * added handlebar views and example json + html
    
    * home view worked on
    
    * formatted files
    
    * fix tabs
    
    * formatted and corrected example json models
    
    Co-authored-by: kcl16 <54655376+kcl16@users.noreply.github.com>
    Co-authored-by: Keith Lahmann <kcl16@students.uwf.edu>
    Co-authored-by: root <root@pop-os.localdomain>
    Co-authored-by: John Chamblee <42752805+JChamblee99@users.noreply.github.com>

---
+++ src/app.js
@@ -0,0 +1,32 @@
+// npm packages
+const express = require('express');
+const app = express();
+
+// set up handlebars view engine
+let handlebars = require('express-handlebars')
+    .create({ defaultLayout: 'main' });
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+
+// middleware
+const cloudflare_middleware = require('./middleware/cloudflare.js');
+const status_middleware = require('./middleware/status_code.js');
+
+app.set('port', process.env.PORT || 3000);
+
+app.use(express.static(__dirname + '/public'));
+
+// Cloudflare isolation handler (middleware)
+app.use(cloudflare_middleware);
+
+app.get('/', (req, res) => {
+    res.render('home');
+});
+
+// 404 catch-all handler (middleware)
+app.use(status_middleware.status_404);
+
+// 500 error handler (middleware)
+app.use(status_middleware.status_500);
+
+module.exports = app;
