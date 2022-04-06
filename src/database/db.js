355d42fded9e072dfd81e5f1e1fdad4f359060ec
jwm52 <jwm52@students.uwf.edu>
2022-04-06 15:49:48 -0500

    cloudflare tweak

--- src/database/db.js
+++ src/database/db.js
@@ -3,7 +3,7 @@ var credentials = require('./credentials');
 
 // Choose which URL to use for this connection
 let dbURI = credentials.mongo.development.connectionString;
-if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'staging') {
+if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'branch') {
   dbURI = credentials.mongo.production.connectionString;
 }
 
