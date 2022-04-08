7cd0451fed22aca1a049f76753232979e11b50ee
jwm52 <jwm52@students.uwf.edu>
2022-04-08 18:43:07 -0500

    refactored db.js to style guide

--- src/database/db.js
+++ src/database/db.js
@@ -1,54 +1,54 @@
 const mongoose = require('mongoose');
 var credentials = require('./credentials');
 
-// Choose which URL to use for this connection
-// let dbURI = credentials.mongo.development.connectionString;
-let dbURI = "";
-if (process.env.MONGODB_URI) {
-  dbURI = credentials.mongo.production.connectionString;
+let dbURI = "Dev DB";
+if (process.env.MONGODB_URI) { // Use Remote DB
+    dbURI = credentials.mongo.production.connectionString;
 
-  // Mongoose connection options
-  let options = { useUnifiedTopology: true, useNewUrlParser: true };
+    // Mongoose connection options
+    let options = { useUnifiedTopology: true, useNewUrlParser: true };
 
-  mongoose.connect(dbURI, options);
+    mongoose.connect(dbURI, options);
 
-} else {
-  const { dbConnect, dbDisconnect } = require('../../utils/test-utils/dbHandler.utils');
-  dbConnect();
+} else { // Use Local DB
+    const { dbConnect, dbDisconnect } = require('../../utils/test-utils/dbHandler.utils');
+    dbConnect();
 }
 
 mongoose.connection.on('connected', () => {
-  console.log(`Mongoose connected to ${dbURI}`);
+    console.log(`Mongoose connected to ${dbURI}`);
 });
 mongoose.connection.on('error', err => {
-  console.log('Mongoose connection error:', err);
+    console.log('Mongoose connection error:', err);
 });
 mongoose.connection.on('disconnected', () => {
-  console.log('Mongoose disconnected');
+    console.log('Mongoose disconnected');
 });
 
 const gracefulShutdown = (msg, callback) => {
-  mongoose.connection.close(() => {
-    console.log(`Mongoose disconnected through ${msg}`);
-    callback();
-  });
+    mongoose.connection.close(() => {
+        console.log(`Mongoose disconnected through ${msg}`);
+        callback();
+    });
 };
 
 // For nodemon restarts                                 
 process.once('SIGUSR2', () => {
-  gracefulShutdown('nodemon restart', () => {
-    process.kill(process.pid, 'SIGUSR2');
-  });
+    gracefulShutdown('nodemon restart', () => {
+        process.kill(process.pid, 'SIGUSR2');
+    });
 });
+
 // For app termination
 process.on('SIGINT', () => {
-  gracefulShutdown('app termination', () => {
-    process.exit(0);
-  });
+    gracefulShutdown('app termination', () => {
+        process.exit(0);
+    });
 });
+
 // For Heroku app termination
 process.on('SIGTERM', () => {
-  gracefulShutdown('Heroku app shutdown', () => {
-    process.exit(0);
-  });
+    gracefulShutdown('Heroku app shutdown', () => {
+        process.exit(0);
+    });
 });
