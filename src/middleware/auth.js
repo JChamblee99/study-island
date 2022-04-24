1f47035f68eea61f2aa430b8767ff7821a01b651
JChamblee99 <jtc38@students.uwf.edu>
2022-04-24 08:05:04 -0500

    Added island mod/user auth middleware

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,7 +1,44 @@
-module.exports.isLoggedIn = (req, res, next) => {
-    if (req.user) {
-        next();
-    } else {
-        res.redirect('/auth/login');
+require('../database/models/island.model');
+require('../database/models/user.model');
+
+const mongoose = require('mongoose');
+const Island = mongoose.model("Island");
+
+module.exports = {
+
+    isLoggedIn: (req, res, next) => {
+        if (req.user) {
+            next();
+        } else {
+            res.redirect('/auth/login');
+        }
+    },
+
+    isIslandModerator: async (req, res, next) => {
+        const island = await Island.findById(req.params.islandId);
+        console.log(island);
+        if (island.mods.includes(req.user._id)) {
+            next();
+        } else {
+            res.status(403);
+            res.json({
+                status: "error",
+                data: "Permission denied"
+            });
+        }
+    },
+
+    isIslandUser: async (req, res, next) => {
+        const island = await Island.findById(req.params.islandId);
+        if (island.users.includes(req.user._id)) {
+            next();
+        } else {
+            res.status(403);
+            res.json({
+                status: "error",
+                data: "Permission denied"
+            });
+        }
     }
 };
+
