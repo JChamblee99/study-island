44d4a5b16719a42ab4418a3b53f5f3a47d98fa5c
Keith Lahmann <kcl16@students.uwf.edu>
2022-04-28 10:46:32 -0500

    added search route with middleware

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,11 +2,14 @@ const express = require('express');
 const router = express.Router();
 
 const homeController = require('../controllers/index');
+const auth_middleware = require('../middleware/auth');
 
 /* Home Routes*/
 //Home page
 router.get('/', (req, res) => {
 	res.render('home');
 });
+
+router.get('/search', auth_middleware.isLoggedIn, homeController.Search);
 
 module.exports = router;
